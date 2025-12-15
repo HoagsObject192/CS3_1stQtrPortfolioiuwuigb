1b0340f4e48442912e79072246f3515d660e2db3
HoagsObject192 <zaphyschoolacc@gmail.com>
2025-12-15 14:47:16 +0800

    tgbrdf

---
+++ rtg.js
@@ -0,0 +1,14 @@
+// Starter code: Read input and echo it back
+// This verifies the code runner is working!
+
+function solve() {
+const m = Number(readln());
+
+let o = [];
+
+for (let q = 0; q < m; q++) {
+    o.push(readln());
+
+}
+
+solve();
